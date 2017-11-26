ffe48a15c472cfb62c4b19d173101d53d803f9dc
Ismayil Tahmazov <tehmezovismayil@gmail.com>
2017-11-26 14:05:43 +0300

    Update fakemails.php
    Update new mail list.

--- fakemails.php
+++ fakemails.php
@@ -17,7 +17,15 @@ $FakeMailList = array(
 	"datum2.com","geronra.com","asorent.com","ether123.net",
 	"ququb.com","2anom.com","qwfox.com","hvzoi.com",
 	"fakeinbox.info","trash-mail.com","you-spam.com","re-gister.com",
-	"fake-box.com","trash-me.com","opentrash.com"
+	"fake-box.com","trash-me.com","opentrash.com","dropmail.me","emlhub.com",
+	"emltmp.com","emlpro.com","yomail.info","10mail.org","mvrht.net",
+	"kopqi.com","fxe.us","gicua.com","20mail.eu","10minutemail.co.uk",
+	"vmani.com","wimsg.com","my10minutemail.com","10minutesmail.fr",
+	"10minutemail.pro","10minutemailbox.com","10minutemail.be",
+	"guerrillamail.biz","sharklasers.com","guerrillamail.info","grr.la",
+	"guerrillamail.com","guerrillamail.de","guerrillamail.net","guerrillamail.org",
+	"guerrillamailblock.com","pokemail.net","spam4.me","10minutemail.nl",
+	"10minute-email.com"
 );
 
 function checkfakemail( $mail )
